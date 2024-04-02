98bff49d25b6423904fee21b63944bb91b9e9468
Pol <polpeidie@campus.monlau.com>
2024-04-02 16:17:47 +0200

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'products', component: ProductsComponent },
+    { path: 'admin', component: AdminComponent },
+    { path: '**', redirectTo: 'home', pathMatch: 'full' },
+];
