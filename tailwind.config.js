9acf296cb203aff201d15f5c67acd8882b293c2e
Pol <polpeidie@campus.monlau.com>
2024-04-03 19:52:13 +0200

    Header navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}",],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Helvetica Now Text', 'Helvetica', 'Arial', 'sans-serif']
+      },
+      fontWeight: {
+        '100': '100;',
+        '200': '200;',
+        '300': '300;',
+        '400': '400;',
+        '500': '500;',
+        '600': '600;',
+        '700': '700;',
+        '800': '800;',
+        '900': '900;',
+      }
+    },
   },
   plugins: [],
 }
